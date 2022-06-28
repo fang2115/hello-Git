5e8f111aa56eff0fc4fc0a7a7faaa804b9c6373d
Jaafar <732868006@qq.com>
2022-06-28 21:45:04 +0800

    spring

--- src/main/java/com/fang/service/AppConfig.java
+++ src/main/java/com/fang/service/AppConfig.java
@@ -4,5 +4,7 @@ import com.fang.spring.ComponentScan;
 //通过注解获得扫描路径
 @ComponentScan("com.fang.service")
 public class AppConfig {
-
+    public static void main(String[] args) {
+        System.out.println("66666");
+    }
 }
